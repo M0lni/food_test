35778fcdc8b104ad14605d2a857227f9b2a9393c
Your Name <you@example.com>
2021-11-06 19:31:52 +0100

    v 2.0

--- food.test.js
+++ food.test.js
@@ -18,4 +18,32 @@ describe('Food tests', () => {
 
         expect(postResponse.code).toBe(400)
     })
+    it('returns error fornegative calories', async () => {
+
+        const postResponse = await client.post('/api/food', {'name':'krumpli','calories':-20})
+
+        expect(postResponse.code).toBe(400)
+    })
+    it('can return created food', async () => {
+        let sajt = {'name': 'sajt', 'calories':50}
+        let alma = {'name': 'alma', 'calories': 10}
+        
+        const sajtpostResponse = await client.post('/api/food', sajt)
+        const almapostResponse = await client.post('/api/food', alma)
+
+        const sajtResponse = await client.post('/api/food', sajt)
+        const sajtId = JSON.parse(sajtResponse.body).id
+        const almaResponse = await client.post('/api/food', alma)
+        const almaId = JSON.parse(almaResponse.body).id
+
+        const getResponse = await client.get('/api/food')
+        expect(getResponse.code).toBe(200)
+
+        const getResponseBody = JSON.parse(getResponse.body)
+        sajt.id = sajtId
+        alma.id = almaId
+
+        expect(getResponseBody).toContainEqual(sajt)
+        expect(getResponseBody).toContainEqual(alma)
+    })
 })
